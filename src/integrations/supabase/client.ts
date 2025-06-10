e5094de962a8de0a431e39bf6455263c8baa0cce
James Campbell <james.campbell@tanti.org.uk>
2025-06-10 12:35:15 +0100

    New supabase config

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://sygcohrlgvqzbazzslbh.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5Z2NvaHJsZ3ZxemJhenpzbGJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ0MzY1MDAsImV4cCI6MjA1MDAxMjUwMH0.WHg79V2sFsGhg4AENlW7K2PIfTPdJbqCsFQSX-Za86c";
+const SUPABASE_URL = "https://ybsrjbbxpwtgcfttrfzs.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlic3JqYmJ4cHd0Z2NmdHRyZnpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk1NTM2NTksImV4cCI6MjA2NTEyOTY1OX0.TJrXCFgF7Ctp9qJXgXArBUc-_M7UwtmmOfgbIpmbWVk";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
